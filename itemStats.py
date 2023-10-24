e8b559c721829c41a12ab327d106f782f3956538
Albin-uu <144899343+Albin-uu@users.noreply.github.com>
2023-10-24 12:23:30 +0200

    Lade till itemStats fil
    Får diskutera hur det ska implementeras.
    Meningen är att ha all item-datan i denna fil, sedan ta bort datan i huvudfil för att i stället importera.

---
+++ itemStats.py
@@ -0,0 +1,6 @@
+# Main file will import this to get buy/sell value, 
+# info, interaction with specific item, stats for chosen item, etc.
+
+# Structure:
+# Use specific item function, eg function for cracking geode
+# Dictionary of item statistics, 
